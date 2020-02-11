51432a34e8c74d254a55c13dee80fa97d69a03df
farleybsd <farley.t.i@hotmail.com>
2020-02-11 20:39:32 -0300

    Apredendo Query params

--- modulo01/index.js
+++ modulo01/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const server = express()
 
+// Query params = ?teste=1
+// Route params = /users/1
+//Request body = {"name": "Farley Rufino", "e-mail": "farley.t.i@hotmail.com"}
+
+
 server.get('/teste',(req,res) =>{
-    return res.json({msg:"hello word"})
+    const nome = req.query.nome
+    return res.json({msg:`Hello ${nome}`})
 })
 
 server.listen(3000,function(){
