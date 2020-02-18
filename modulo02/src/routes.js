47817c0008cc85ceb241c050f11b71ec64c07014
farleybsd <farley.t.i@hotmail.com>
2020-02-17 21:44:41 -0300

    debug nodemon sucrase

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,11 +1,10 @@
-const {Router} = require('express')
+import {Router}  from'express'
 
 const routes =  new Router()
 
 routes.get('/',(req,res)=>{
-    return res.json({message: "Hello Word"})
+    return res.json({message: "Hello Farley ;)"})
 })
 
 
-
-module.exports = routes
+export default routes
