adf961d2bd4f4a08541a7f3ab2bea474ace0aa6a
farleybsd <farley.t.i@hotmail.com>
2020-02-20 21:56:39 -0300

    Instalado Squelize

---
+++ modulo02/src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    database: 'gobarber',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+    }
+
+}
