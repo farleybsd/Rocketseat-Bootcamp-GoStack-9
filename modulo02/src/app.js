8f00d35efd5f566f72e9a693da9632a879ef44e9
farleybsd <farley.t.i@hotmail.com>
2020-04-03 21:43:57 -0300

    21 - Tratamento de exceções

--- modulo02/src/app.js
+++ modulo02/src/app.js
@@ -1,23 +1,43 @@
+
 import express from "express";
 import path from 'path'
+import Youch from 'youch'
+import * as Sentry from '@sentry/node'
+import 'express-async-errors'
 import routes from "./routes";
+import SentryConfig from './config/sentry'
 import './database';
 
 class App {
     constructor() {
         this.sever = express();
+        Sentry.init(SentryConfig)
         this.middlewares();
         this.routes();
+        this.exceptionHandler()
     }
 
     middlewares() {
+        this.sever.use(Sentry.Handlers.requestHandler());
         this.sever.use(express.json());
         this.sever.use('/flies', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
     }
 
     routes() {
         this.sever.use(routes);
+        this.sever.use(Sentry.Handlers.errorHandler());
     }
+
+    exceptionHandler() {
+        this.sever.use(async (err, req, res, next) => {
+
+            const erros = await new Youch(err, req).toJSON()
+
+            return res.status(500).json(erros)
+
+        })
+    }
+
 }
 
 export default new App().sever;
---
+++ modulo02/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dsn: 'https://8177e9664fd849798a65a08438c7a897@sentry.io/5188345'
+}
