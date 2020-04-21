a356878ac08e384e1bd81839caa98d207802b4c0
farleybsd <farley.t.i@hotmail.com>
2020-04-21 11:10:14 -0300

    04 - Styled Components

--- modulo05/modulo05/src/pages/Main/index.js
+++ modulo05/modulo05/src/pages/Main/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Title } from './styles';
 
 export default function Main() {
     return (
-        <h1>Main</h1>
+        <Title error>
+            Main
+            <small>menor</small>
+        </Title>
     );
 }
