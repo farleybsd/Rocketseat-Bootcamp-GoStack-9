a58f5a56586a7cce4e4a2d534efb90bc4d3ccd46
farleybsd <farley.t.i@hotmail.com>
2020-04-23 19:20:44 -0300

    14 - Exibindo issues

--- modulo05/modulo05/src/pages/Repository/index.js
+++ modulo05/modulo05/src/pages/Repository/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import api from '../../services/api'
 
 import Container from '../../components/Container'
-import { Loading, Ownver } from './styles'
+import { Loading, Ownver, IssueList } from './styles'
 export default class Repository extends Component {
 
     static propTypes = {
@@ -62,12 +62,33 @@ export default class Repository extends Component {
         return (
 
             <Container>
+
                 <Ownver>
                     <Link to="/">Voltar aos repositórios </Link>
                     <img src={repository.owner.avatar_url} alt={repository.owner.login}></img>
                     <h1>{repository.name}</h1>
                     <p>{repository.description}</p>
                 </Ownver>
+
+                <IssueList>
+                    {issues.map(issue => (
+                        <li key={String(issue.id)}>
+
+                            <img src={issue.user.avatar_url} alt={issue.user.login}></img>
+                            <div>
+                                <strong>
+                                    <a href={issue.html_url}>{issue.title}</a>
+                                    {issue.labels.map(label => (
+                                        <span Key={String(label.id)}>{label.name}</span>
+                                    ))}
+                                </strong>
+                                <p>{issue.user.login}</p>
+                            </div>
+
+                        </li>
+                    ))}
+                </IssueList>
+
             </Container>
 
         )
