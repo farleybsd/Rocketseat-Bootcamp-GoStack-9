aaa6b70ca9403ef25418651c734651966a4b4506
farleybsd <farley.t.i@hotmail.com>
2020-04-27 18:44:20 -0300

    Style carrinho

--- modulo06/redux/src/pages/Cart/index.js
+++ modulo06/redux/src/pages/Cart/index.js
@@ -1,8 +1,66 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md'
 
-// import { Container } from './styles';
+import { Container, ProductTABLE, Total } from './styles';
 
 export default function Cart() {
-    return <h1>Carrino</h1>;
+    return (
+
+        <Container>
+            <ProductTABLE>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>Produto</th>
+                        <th>Quantidade</th>
+                        <th>SubTotal</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
+                                alt="Tênis Top" />
+                        </td>
+                        <td>
+                            <strong>Tênis muito massa</strong>
+                            <span>R$129,90</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                                </button>
+
+                                <input type="number" readOnly value={2} />
+
+                                <button type="button">
+                                    <MdAddCircleOutline size={20} color="#7159c1" />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$258,80</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTABLE>
+
+            <footer>
+                <button type="button">Finalizar Pedido</button>
+                <Total>
+                    <span>Total</span>
+                    <strong>R$1920,28</strong>
+                </Total>
+            </footer>
+        </Container>
+
+    )
 
 }
