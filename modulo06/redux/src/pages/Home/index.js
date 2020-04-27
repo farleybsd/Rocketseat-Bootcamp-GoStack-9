186f478f40d666a33ff1aadd70ad1f3490221b06
farleybsd <farley.t.i@hotmail.com>
2020-04-27 16:52:25 -0300

    Estilizando a Home

--- modulo06/redux/src/pages/Home/index.js
+++ modulo06/redux/src/pages/Home/index.js
@@ -1,9 +1,120 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md'
+import { ProductList } from './styles';
 
 export default function Home() {
     return (
-        <div />
+        <ProductList>
+            <li>
+                <img
+
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
+                    alt="Tênis"
+
+                />
+
+                <strong>Tênis Legal Demais</strong>
+                <span>R$129,99</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+                    <span>Adiconar Ao Carrinho</span>
+                </button>
+            </li>
+
+            <li>
+                <img
+
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
+                    alt="Tênis"
+
+                />
+
+                <strong>Tênis Legal Demais</strong>
+                <span>R$129,99</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+                    <span>Adiconar Ao Carrinho</span>
+                </button>
+            </li>
+
+
+            <li>
+                <img
+
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
+                    alt="Tênis"
+
+                />
+
+                <strong>Tênis Legal Demais</strong>
+                <span>R$129,99</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+                    <span>Adiconar Ao Carrinho</span>
+                </button>
+            </li>
+
+            <li>
+                <img
+
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
+                    alt="Tênis"
+
+                />
+
+                <strong>Tênis Legal Demais</strong>
+                <span>R$129,99</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+                    <span>Adiconar Ao Carrinho</span>
+                </button>
+            </li>
+
+            <li>
+                <img
+
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
+                    alt="Tênis"
+
+                />
+
+                <strong>Tênis Legal Demais</strong>
+                <span>R$129,99</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+                    <span>Adiconar Ao Carrinho</span>
+                </button>
+            </li>
+
+            <li>
+                <img
+
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
+                    alt="Tênis"
+
+                />
+
+                <strong>Tênis Legal Demais</strong>
+                <span>R$129,99</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+                    <span>Adiconar Ao Carrinho</span>
+                </button>
+            </li>
+
+        </ProductList>
     );
 }
