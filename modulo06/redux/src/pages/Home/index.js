5360020ec00e3a69084815f359e172ac22e1cb2a
farleybsd <farley.t.i@hotmail.com>
2020-04-28 22:05:47 -0300

    List Products Api

--- modulo06/redux/src/pages/Home/index.js
+++ modulo06/redux/src/pages/Home/index.js
@@ -1,120 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { MdAddShoppingCart } from 'react-icons/md'
+import { formatPrice } from '../../util/format'
+import api from '../../services/api'
 import { ProductList } from './styles';
 
-export default function Home() {
-    return (
-        <ProductList>
-            <li>
-                <img
+export default class Home extends Component {
 
-                    src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
-                    alt="Tênis"
+    state = {
+        products: []
+    }
 
-                />
+    async componentDidMount() {
 
-                <strong>Tênis Legal Demais</strong>
-                <span>R$129,99</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>Adiconar Ao Carrinho</span>
-                </button>
-            </li>
+        const response = await api.get('products')
 
-            <li>
-                <img
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormatted: formatPrice(product.price),
+        }))
 
-                    src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
-                    alt="Tênis"
+        this.setState({ products: data })
 
-                />
+    }
 
-                <strong>Tênis Legal Demais</strong>
-                <span>R$129,99</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>Adiconar Ao Carrinho</span>
-                </button>
-            </li>
+    render() {
+
+        const { products } = this.state;
+
+        return (
+            <ProductList>
+                {products.map(product => (
+
+                    <li Key={String(product.id)}>
+                        <img
+
+                            src={product.image}
+                            alt={product.title}
+
+                        />
+
+                        <strong>{product.title}</strong>
+                        <span>{product.priceFormatted}</span>
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart size={16} color="#fff" /> 3
+                            </div>
+                            <span>Adiconar Ao Carrinho</span>
+                        </button>
+                    </li>
+
+                ))}
 
 
-            <li>
-                <img
 
-                    src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
-                    alt="Tênis"
 
-                />
 
-                <strong>Tênis Legal Demais</strong>
-                <span>R$129,99</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>Adiconar Ao Carrinho</span>
-                </button>
-            </li>
-
-            <li>
-                <img
-
-                    src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
-                    alt="Tênis"
-
-                />
-
-                <strong>Tênis Legal Demais</strong>
-                <span>R$129,99</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>Adiconar Ao Carrinho</span>
-                </button>
-            </li>
-
-            <li>
-                <img
-
-                    src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
-                    alt="Tênis"
-
-                />
-
-                <strong>Tênis Legal Demais</strong>
-                <span>R$129,99</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>Adiconar Ao Carrinho</span>
-                </button>
-            </li>
-
-            <li>
-                <img
-
-                    src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
-                    alt="Tênis"
-
-                />
-
-                <strong>Tênis Legal Demais</strong>
-                <span>R$129,99</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>Adiconar Ao Carrinho</span>
-                </button>
-            </li>
-
-        </ProductList>
-    );
+            </ProductList>
+        );
+    }
 }
