4e79badd0554a45f41e4593d82f6a702c0df5343
farleybsd <farley.t.i@hotmail.com>
2020-05-01 14:05:37 -0300

    13 - Listando no carrinho

--- modulo06/redux/src/pages/Cart/index.js
+++ modulo06/redux/src/pages/Cart/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md'
 
 import { Container, ProductTABLE, Total } from './styles';
 
-export default function Cart() {
+function Cart({ cart }) {
     return (
 
         <Container>
@@ -18,37 +19,39 @@ export default function Cart() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            <img src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/82/COL-6981-982/COL-6981-982_zoom1.jpg?ts=1584624435&ims=544x"
-                                alt="Tênis Top" />
-                        </td>
-                        <td>
-                            <strong>Tênis muito massa</strong>
-                            <span>R$129,90</span>
-                        </td>
-                        <td>
-                            <div>
-                                <button type="button">
-                                    <MdRemoveCircleOutline size={20} color="#7159c1" />
-                                </button>
+                    {cart.map(product => (
+                        <tr>
+                            <td>
+                                <img src={product.image}
+                                    alt={product.title} />
+                            </td>
+                            <td>
+                                <strong>{product.title}</strong>
+                                <span>{product.priceFormatted}</span>
+                            </td>
+                            <td>
+                                <div>
+                                    <button type="button">
+                                        <MdRemoveCircleOutline size={20} color="#7159c1" />
+                                    </button>
 
-                                <input type="number" readOnly value={2} />
+                                    <input type="number" readOnly value={product.amount} />
 
+                                    <button type="button">
+                                        <MdAddCircleOutline size={20} color="#7159c1" />
+                                    </button>
+                                </div>
+                            </td>
+                            <td>
+                                <strong>R$258,80</strong>
+                            </td>
+                            <td>
                                 <button type="button">
-                                    <MdAddCircleOutline size={20} color="#7159c1" />
+                                    <MdDelete size={20} color="#7159c1" />
                                 </button>
-                            </div>
-                        </td>
-                        <td>
-                            <strong>R$258,80</strong>
-                        </td>
-                        <td>
-                            <button type="button">
-                                <MdDelete size={20} color="#7159c1" />
-                            </button>
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </ProductTABLE>
 
@@ -64,3 +67,10 @@ export default function Cart() {
     )
 
 }
+
+const mapStateToProps = state => ({
+
+    cart: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart)
